7231f678937c0be6869ea09ed90deeae4e4c41db
alrik11es <alrik11es@gmail.com>
2012-11-13 11:12:55 +0100

    Added two files.

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'src/Nano.php';
+
+$nano = new Nano('http://localhost:5984');
+//$err = $nano->db->create('alice');
+
+//$nano->db->create('alice');
+$nano->db->destroy('alice');
+$nano->db->list();
